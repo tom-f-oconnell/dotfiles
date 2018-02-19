301c0ba96d3181a8319293413484a58e462e98fb
Tom O'Connell <toconnel@caltech.edu>
2018-02-18 23:53:41 -0800

    Changed remote URL for scripts. Assuming you can clone from this, it won't require changing later for pushing via SSH.

--- get_scripts.sh
+++ get_scripts.sh
@@ -4,7 +4,7 @@ mkdir -p ~/src
 cd ~/src
 
 if [ ! -d ~/src/scripts ] ; then
-    git clone git://github.com/tom-f-oconnell/scripts.git
+    git clone git@github.com:tom-f-oconnell/scripts.git
 else
     cd scripts
     git pull --rebase || (exit 0)
