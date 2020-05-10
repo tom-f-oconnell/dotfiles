4693d39fe2d94fa5e398b79bc300495807accb3d
Tom O'Connell <toconnel@caltech.edu>
2020-05-10 00:10:19 -0700

    Fixed ssh_config permission bug in already-installed WSL case. Changed TODOs after testing. Improved VIM ext install script.

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 
-See [dotbot](https://github.com/anishathalye/dotbot).
+I added a `link_only` script, which will link all of the configuration files
+without running the additional installation steps that `install` does.
 
-I added a `link_only` script, which will not run the additional installation
-scripts that `install` does.
+Current steps to deploy only config links + VIM extensions on WSL:
+1. See TODO.txt for notes on parts of WSL config I have not automated. 
+   Changing the home folder is among the important such steps.
+2.
+```
+git clone git@github.com:tom-f-oconnell/dotfiles
+cd dotfiles
+./link_only
+./install_vim_extensions.sh
+```
+
+Upstream for this fork is [dotbot](https://github.com/anishathalye/dotbot).
 
--- install_vim_extensions.sh
+++ install_vim_extensions.sh
@@ -2,38 +2,33 @@
 
 # TODO maybe also allow overriding this if called from main install.conf.yaml
 # instructions, and the install script that does this precedes
-# TODO test
 # https://stackoverflow.com/questions/1885525
-read -p "'sudo apt-get update' before installing YouCompleteMe deps?" -n 1 -r
+read -p "'sudo apt-get update' before installing YouCompleteMe deps? (y/n) " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     sudo apt-get update
 fi
 
-# TODO did it matter that this script was called w/ sudo in dotbot and no sudo
-# in here?
-
 # TODO test this works on fresh 16.04, 18.04, and WSL
 # (see old version of this file when it was named ycm_deps.sh if those
 # deps work better than these ones)
 sudo apt-get install -y build-essential cmake python3-dev
 
+# TODO modify so vim doesn't show output here if possible (or not interactive)
+# (so vim doesn't spit out "Warning: Output is not to a terminal" when called
+# from `install`
 # https://github.com/VundleVim/Vundle.vim
 vim +PluginInstall +qall
 
 # Important it happens after the vim call above, because that's when Vundle will
 # download the YouCompleteMe source.
 # Just assuming script called from `dotfiles` root directory for now.
-# TODO check that this cd doesn't break calling script (it probably shouldn't)
-# (doing it this way b/c i feel compilation script may expect that)
 cd vim/bundle/YouCompleteMe
-
-# TODO maybe use --quiet option?
 
 # Using this option rather than --all b/c not interested in other languages
 # explicitly mentioned in --help output, and some can cause compilation to fail,
-# like C#. Doesn't seem like it should affect Python support?
+# like C#. Python support still works for me in WSL w/ these options.
 # Maybe just call with no options?
-./install.py --clang-completer
+./install.py --clang-completer --quiet
 
